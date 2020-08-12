33653710ccd9a6390bd161a372f089431724ebf0
kubus0750 <kubus0750@gmail.com>
2020-08-12 10:48:02 +0200

    0.1

--- express-api/config/config.js
+++ express-api/config/config.js
@@ -1,6 +1,7 @@
 var config = {};
 
-config.dbhost = process.env.DBHOST || 'mongodb://localhost/appointment';
+config.dbhost =
+	process.env.DBHOST || 'mongodb+srv://jakub:Kubacek2010@cluster-jakub-qb3js.mongodb.net/appointment?retryWrites=true&w=majority';
 config.host = process.env.HOST || 'http://localhost';
 
 module.exports = config;
